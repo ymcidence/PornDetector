8ee0096fa7ec9feae2b42d53d2fb72bb89878475
ymcidence <yuming.shen@inceptioniai.org>
2019-10-22 11:33:57 +0400

    initial

--- ym/data.py
+++ ym/data.py
@@ -1,5 +1,48 @@
 from __future__ import print_function, absolute_import, division
 import tensorflow as tf
-import random
-def make_data():
-    
+import pathlib
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+IMG_SIZE = 160  # All images will be resized to 160x160
+IMG_SHAPE = (IMG_SIZE, IMG_SIZE, 3)
+
+
+def preprocess_image(image):
+    image = tf.image.decode_jpeg(image, channels=3)
+    image = tf.image.resize(image, [IMG_SIZE, IMG_SIZE])
+    image = (image / 127.5) - 1  # normalize to [0,1] range
+
+    return image
+
+
+def load_and_preprocess_image(path):
+    image = tf.io.read_file(path)
+    return preprocess_image(image)
+
+
+def make_data(batch_size):
+    image_dir = '/home/ymcidence/Workspace/Data/totalk/'
+    classes = ['blood', 'violence', 'no_voilence']
+    labels = []
+    images = []
+
+    for i, c in enumerate(classes):
+        image_root = pathlib.Path(image_dir + c)
+        for f in image_root.iterdir():
+            images.append(f)
+            labels.append(i)
+
+    images = [str(img) for img in images]
+    image_count = len(images)
+    path_ds = tf.data.Dataset.from_tensor_slices(images)
+
+    image_ds = path_ds.map(load_and_preprocess_image, num_parallel_calls=AUTOTUNE)
+    label_ds = tf.data.Dataset.from_tensor_slices(tf.cast(labels, tf.int32))
+
+    dataset = tf.data.Dataset.zip((image_ds, label_ds))
+
+    return dataset.shuffle(buffer_size=image_count).repeat().batch(batch_size).prefetch(buffer_size=AUTOTUNE)
+
+
+if __name__ == '__main__':
+    make_data(7)
--- ym/model.py
+++ ym/model.py
@@ -1,6 +1,7 @@
 from __future__ import print_function, absolute_import, division
 
 import tensorflow as tf
+from ym.data import make_data, AUTOTUNE
 
 IMG_SIZE = 160  # All images will be resized to 160x160
 IMG_SHAPE = (IMG_SIZE, IMG_SIZE, 3)
@@ -19,9 +20,15 @@ model = tf.keras.Sequential([
     global_average_layer,
     prediction_layer
 ])
-base_learning_rate = 1e-4
-model.compile(optimizer=tf.keras.optimizers.RMSprop(lr=base_learning_rate),
-              loss='categorical_crossentropy',
+base_learning_rate = 1e-3
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=base_learning_rate),
+              loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
 print(model.summary())
+
+dataset = make_data(128)
+
+model.fit(dataset, epochs=1, steps_per_epoch=10000)
+
+tf.keras.experimental.export_saved_model(model, '../bv_model')
